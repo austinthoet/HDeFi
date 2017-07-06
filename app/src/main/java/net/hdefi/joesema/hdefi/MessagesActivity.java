ecaefa156fa9a3980f7c3fff4cfbfb76aacc746d
jps5775 <doej4566@gmail.com>
2017-07-06 17:17:17 -0400

    added Google Firebase Backend connectivity added dependencies for database, storage adjusted LoginActivity added + icon for posts on MessagesActivity

--- app/src/main/java/net/hdefi/joesema/hdefi/MessagesActivity.java
+++ app/src/main/java/net/hdefi/joesema/hdefi/MessagesActivity.java
@@ -3,6 +3,7 @@ package net.hdefi.joesema.hdefi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -53,5 +54,13 @@ public class MessagesActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
